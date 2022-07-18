a57f1ae2f37b9c1c18728e54f739439f79275f84
Maciej Druciarek <kobraks2@gmail.com>
2022-07-18 12:10:50 +0200

    Added missing files to premake

--- premake5.lua
+++ premake5.lua
@@ -4,20 +4,22 @@ project "ImGui"
 	cppdialect "C++20"
 	staticruntime "off"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
 		"imconfig.h",
 		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
 		"imgui_internal.h",
-		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
+
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
 		"imgui_demo.cpp"
 	}
 
